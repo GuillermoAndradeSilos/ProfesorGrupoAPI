641110c7df103d4b2a197dacb7ffb5655bca39bb
Sweetx <memosaul.a@hotmail.com>
2023-03-11 20:48:21 -0600

    Cadena de conexion actualizada

--- ProfesorGrupoAPI/Program.cs
+++ ProfesorGrupoAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<Sistem21PrimariaContext>(
-    optionsBuilder => optionsBuilder.UseMySql("server=sistemas19.com;database=sistem21_primaria;user=sistem21_primaria;password=sistemas19_", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.17-mariadb"))
+    optionsBuilder => optionsBuilder.UseMySql("server=sistemas19.com;database=sistem21_primaria;user=sistem21_Test;password=sistemas20_", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.5.17-mariadb"))
     );
 var app = builder.Build();
 app.UseRouting();
